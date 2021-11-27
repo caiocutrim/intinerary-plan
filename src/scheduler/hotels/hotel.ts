81c725c69e6ae50569d04c81cc7520e168deb5ce
caio pablo  veloso cutrim <caio.cutrim@midway.com.br>
2021-11-27 17:13:27 -0300

    add @nestjs/schedule library and initial setup

--- src/scheduler/hotels/hotel.ts
+++ src/scheduler/hotels/hotel.ts
@@ -1,1 +1,12 @@
-export class Hotel {}
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+
+@Injectable()
+export class Hotel {
+  private readonly logger = new Logger(Hotel.name);
+
+  @Cron('10 * * * * *')
+  getHotels() {
+    this.logger.debug('I am ready to get hotels from third part api');
+  }
+}
